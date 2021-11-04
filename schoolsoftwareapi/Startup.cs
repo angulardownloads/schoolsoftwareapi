81b212f36a8206421ae115e1d67d11d96271d1af
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-11-04 09:40:19 +0530

    push

--- schoolsoftwareapi/Startup.cs
+++ schoolsoftwareapi/Startup.cs
@@ -35,6 +35,8 @@ namespace schoolsoftwareapi
 
 
             });
+
+          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,13 +44,17 @@ namespace schoolsoftwareapi
         {
 
             app.UseSwagger();
-
+      
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
+               
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = String.Empty;
             });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
