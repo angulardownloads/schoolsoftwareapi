6e3b20089560f3d959b6d9e701cff88412576040
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-11-08 08:15:23 +0530

    push

--- schoolsoftwareapi/Startup.cs
+++ schoolsoftwareapi/Startup.cs
@@ -82,16 +82,16 @@ namespace schoolsoftwareapi
                 endpoints.MapControllers();
             });
 
-            app.Use(async (context, next) =>
-            {
-                if (!context.Request.Path.Value.Contains("/swagger", StringComparison.OrdinalIgnoreCase))
-                {
-                    context.Response.Redirect("swagger");
-                    return;
-                }
+            //app.Use(async (context, next) =>
+            //{
+            //    if (!context.Request.Path.Value.Contains("/swagger", StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        context.Response.Redirect("swagger");
+            //        return;
+            //    }
 
-                await next();
-            });
+            //    await next();
+            //});
         }
     }
 }
