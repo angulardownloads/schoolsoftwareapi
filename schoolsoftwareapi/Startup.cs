ed7f8fa768e343a4571f0f528199123a9966ea41
Ravikumar <ravikumarachari.s@sonata-software.com>
2021-11-08 08:11:54 +0530

    push

--- schoolsoftwareapi/Startup.cs
+++ schoolsoftwareapi/Startup.cs
@@ -60,7 +60,7 @@ namespace schoolsoftwareapi
             {
                
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = "swagger"+String.Empty;
+                c.RoutePrefix = "/swagger"+String.Empty;
             });
 
 
